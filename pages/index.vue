68ac756a60052bbfcc8cb140b58cf955ba7ebfe0
yanase-kenta0505 <ynsknt0505@gmail.com>
2022-03-22 21:32:42 +0900

    編集機能の実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
-    <v-card width="600px" class="mx-auto mt-10 blue white--text" tile elevation="0">
+    <v-card
+      width="600px"
+      class="mx-auto mt-10 blue white--text"
+      tile
+      elevation="0"
+    >
       <p class="text-h3 text-center ma-0">TODO LIST</p>
     </v-card>
 
@@ -10,10 +15,16 @@
       <v-list dense flat>
         <v-subheader class="mb-3 d-flex justify-space-between">
           <div class="text-h6 blue--text ml-2">
-            <p class="ma-0" >{{ todos.length }} Items Left</p>
+            <p class="ma-0">{{ todos.length }} Items Left</p>
           </div>
           <div>
-            <v-btn-toggle tile color="blue align-self-center" group v-model="toggleNum" mandatory>
+            <v-btn-toggle
+              tile
+              color="blue align-self-center"
+              group
+              v-model="toggleNum"
+              mandatory
+            >
               <v-btn value="left">All</v-btn>
               <v-btn value="center">Active</v-btn>
               <v-btn value="right">Completed</v-btn>
@@ -26,13 +37,25 @@
             <v-list-item-action>
               <v-checkbox></v-checkbox>
             </v-list-item-action>
-            <v-list-item-content >
-              <v-list-item-title  class="text-h6">{{ todo.taskName }}</v-list-item-title>
+            <v-list-item-content>
+              <v-text-field
+                id="taskName"
+                autocomplete="off"
+                hide-details="auto"
+                maxlength="20"
+                solo
+                flat
+                height="30px"
+                :outlined="todo.selected"
+                :disabled="!todo.selected"
+                :value="todo.taskName"
+                class="red--text"
+                @blur="changeTaskName(index, $event)"
+                @keydown.enter="changeTaskName(index, $event)"
+              />
             </v-list-item-content>
             <v-list-item-icon>
-              <v-btn icon class="mr-5">
-                <v-icon class="mx-auto">mdi-pencil</v-icon>
-              </v-btn>
+              <EditBtn @editTaskName="editTaskName(index)" />
               <DeleteBtn @deleteItem="deleteItem(index)" />
             </v-list-item-icon>
           </v-list-item>
@@ -46,29 +69,35 @@
 <script>
 import AddTaskField from "../components/addTaskField.vue";
 import DeleteBtn from "../components/deleteBtn.vue";
+import EditBtn from "../components/editBtn.vue";
 
 export default {
-  components: { AddTaskField, DeleteBtn },
+  components: { AddTaskField, DeleteBtn, EditBtn },
 
   data() {
     return {
       todos: [],
-      toggleNum: 'left'
+      toggleNum: "left",
     };
   },
 
   methods: {
     addTodo(newTask) {
-      this.todos.push(newTask)
+      this.todos.push(newTask);
     },
     deleteItem(index) {
-      this.todos.splice(index, 1)
-    }
-  }
-
-}
-
-
+      this.todos.splice(index, 1);
+    },
+    editTaskName(index) {
+      console.log(this.todos);
+      this.todos[index].selected = !this.todos[index].selected;
+    },
+    changeTaskName(index, e) {
+      this.todos[index].taskName = e.target.value;
+      this.todos[index].selected = !this.todos[index].selected;
+    },
+  },
+};
 </script>
 
 <style  scoped>
@@ -82,5 +111,14 @@ export default {
 
 ::v-deep .v-list {
   padding-bottom: 0 !important;
+}
+
+::v-deep .v-ripple__container {
+  display: none !important;
+}
+
+::v-deep .v-input #taskName {
+  font-size: 20px !important;
+  color: blue !important;
 }
 </style>
